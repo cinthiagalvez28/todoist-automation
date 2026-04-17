cab1dd9c4bf360269c7313ce7031ad086fad0360
Cinthia Galvez Echeverria <cinthia.galvez@Cinthias-MacBook-Pro.local>
2026-04-17 09:28:23 -0600

    Manejo de sesion

--- auth.setup.js
+++ auth.setup.js
@@ -5,15 +5,33 @@ const { LoginPage } = require('./pages/login/LoginPage');
 const { ProductsPage } = require('./pages/products/ProductsPage');
 const { USER_CREDENTIALS, DEFAULT_TIMEOUT } = require('./constants/TestData');
 
-// NEEDS IMPROVEMENT
-// if (auth.json existe && sigue válido) return;
+const authPath = path.resolve(__dirname, './auth.json');
+
 module.exports = async () => {
-  const authPath = path.resolve(__dirname, './auth.json');
   if (fs.existsSync(authPath)) {
-    console.log('auth.json ya existe, se reutiliza');
-    return;
+
+    console.info('auth.json existe, validando sesión...');
+    const browser = await chromium.launch();
+    const context = await browser.newContext({storageState: authPath});
+    const page = await context.newPage();
+    try {
+      const productsPage = new ProductsPage(page);
+      await productsPage.goto();
+      const isLoggedIn = await productsPage.productsTitle.isVisible({timeout: DEFAULT_TIMEOUT});
+      if (isLoggedIn) {
+        console.info('Sesión válida, reutilizando auth.json');
+        await browser.close();
+        return;
+      }
+      console.info('Sesión expirada, regenerando auth...');
+      await browser.close();
+    } catch (e) {
+      console.info('Auth inválido, regenerando...');
+      await browser.close();  
+      }
   }
-  console.log('Creando nueva sesión...');
+
+  console.info('Creando nueva sesión...');
   const browser = await chromium.launch();
   const page = await browser.newPage();
   const loginPage = new LoginPage(page);
@@ -24,4 +42,5 @@ module.exports = async () => {
   await page.waitForURL('**/inventory.html');
   await page.context().storageState({ path: authPath });
   await browser.close();
+  console.info('Nuevo auth.json generado');
 };
