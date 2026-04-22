e6c343465fe56db98389065d231c8fc023b4a3e0
Cinthia Galvez Echeverria <cinthia.galvez@Cinthias-MacBook-Pro.local>
2026-04-21 19:50:30 -0600

    Feature Your Information

--- constants/TestData.js
+++ constants/TestData.js
@@ -4,7 +4,9 @@ module.exports = {
     URLS: {
         BASE_URL: process.env.BASE_URL,
         PRODUCTS: '/inventory.html',
-        SHOOPING_CART: '/cart.html'
+        SHOPPING_CART: '/cart.html',
+        YOUR_INFORMATION: '/checkout-step-one.html',
+        OVERVIEW: '/checkout-step-two.html'
     },
     USER_CREDENTIALS: {
         VALID_USER: {
@@ -22,6 +24,13 @@ module.exports = {
                 PASSWORD_IS_REQUIRED: 'Epic sadface: Password is required',
                 USERNAME_IS_REQUIRED: 'Epic sadface: Username is required',
                 USERNAME_AND_PASSWORD_DOES_NOT_MATCH: 'Epic sadface: Username and password do not match any user in this service'
+            }
+        },
+        YOUR_INFORMATION: {
+            ERROR: {
+                FIRST_NAME_IS_REQUIRED: 'Error: First Name is required',
+                LAST_NAME_IS_REQUIRED: 'Error: Last Name is required',
+                POSTAL_CODE_IS_REQUIRED: 'Error: Postal Code is required'
             }
         }
     },
--- tests/logout/logout.spec.js
+++ tests/logout/logout.spec.js
@@ -7,6 +7,7 @@ test.describe('Logout tests', () => {
   test('Successful logout: As a standard user, I should be able to log out.', async ({ productsPage,  loginPage }) => {
     await productsPage.goto();
     await productsPage.navBar.burgerMenuBtn.click();
+    await expect(productsPage.sideBar.logoutBtn).toBeVisible({timeout: DEFAULT_TIMEOUT});
     await productsPage.sideBar.logoutBtn.click();
     await expect(loginPage.loginButton).toBeVisible({timeout: DEFAULT_TIMEOUT});
   });
